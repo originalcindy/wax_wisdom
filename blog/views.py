69e2887c0171c4c6d4eb59dd9041e695734ccbde
Charlene Campbell-Gray <illest.reps@gmail.com>
2024-12-12 21:41:02 +0000

    added settings to views.py and urls.py

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# Index view for the blog
+def index(request):
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
+
+# About page view
+def about_me(request):
+    return HttpResponse("This would be the about page")
